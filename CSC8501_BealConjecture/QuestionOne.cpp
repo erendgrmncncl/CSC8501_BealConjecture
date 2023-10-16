122f99522a9c596c58277bdb08c24ef5d44f0785
eren.degirmenci <eren.degirmenci@inventuna.com>
2023-10-16 10:54:18 +0100

    refactor(logic): changed question one answer logic

--- CSC8501_BealConjecture/QuestionOne.cpp
+++ CSC8501_BealConjecture/QuestionOne.cpp
@@ -5,29 +5,31 @@ namespace {
     constexpr const int BNT_TO_FIND = 5;
 }
 
-QuestionOne::QuestionOne(const char* questionText, std::vector<const char*>& answersText, BealCalculator& bealCalculator) : Question(questionText, answersText, bealCalculator)
-{
+QuestionOne::QuestionOne(const char* questionText, std::vector<const char*>& answersText, BealCalculator& bealCalculator) : Question(questionText, answersText, bealCalculator){
 }
 
 void QuestionOne::findAnswer(){
     int foundBNTCount = 0;
-	for (int A = 3; foundBNTCount < BNT_TO_FIND; A++) {
-        for (int B = A + 1; B <= A * 2; B++) {
-            int C = A + B;
-            for (int x = 3; x < 100; x++) {
-                for (int y = x + 1; y < 100; y++) {
-                    for (int z = y + 1; z < 100; z++) {
-                        if (_bealCalculator->haveCommonPrimeFactor(A, B)) {
-                            BealData* data = new BealData(A, B, C, x, y, z);
-                            _answers.push_back(data);
-                            foundBNTCount++;
-                            if (foundBNTCount == 5)
-                                return;
+	for (int A = 2; A < 100; A++) {
+        for (int B = 2; B < 100; B++){
+            for (int x = 3; x < 100; x++){
+                for (int y = 3; y < 100; y++){
+                    for (int C = 2; C < 100; C++){
+                        for (int z = 3; z < 100; z++){
+                            if (_bealCalculator->isNumberSetFitsBealConjecture(A, B, C, x, y, z)){
+                                BealData* data = new BealData(A, B, C, x, y, z);
+                                _answers.push_back(data);
+                                foundBNTCount++;
+                                if (foundBNTCount == BNT_TO_FIND) {
+                                    return;
+                                }
+                            }
                         }
                     }
                 }
             }
         }
+      
     }
 
 
