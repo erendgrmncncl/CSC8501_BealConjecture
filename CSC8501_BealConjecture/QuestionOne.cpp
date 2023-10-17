1d18d56a15cddd65d089c0dba2448c4e0fcf1665
erendegirmenci <S.E.Degirmenci2@newcastle.ac.uk>
2023-10-17 15:02:20 +0100

    feat(logic): added question one and two answers

--- CSC8501_BealConjecture/QuestionOne.cpp
+++ CSC8501_BealConjecture/QuestionOne.cpp
@@ -10,23 +10,30 @@ QuestionOne::QuestionOne(const char* questionText, std::vector<const char*>& ans
 
 void QuestionOne::findAnswer(){
     std::vector<BealData> bnts;
-	
-    for (int A = 2; A < 10; A++) {
-        for (int B = 2; B < 10; B++){
-            for (int x = 3; x < 10; x++){
-                for (int y = 3; y < 10; y++){
-                    for (int C = 2; C < 10; C++){
-                        for (int z = 3; z < 10; z++){
-                            if (_bealCalculator->isNumberSetFitsBealConjecture(A, B, C, x, y, z)) {
-                                BealData bealData(A, B, C, x, y, z);
-                                _bealCalculator->checkAndAddBNTToMinimumVec(bnts, bealData, BNT_TO_FIND);
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
+
+	for (int A = 2; A < 10; A++) {
+		for (int B = 2; B < A + 1; B++) {
+			if (_bealCalculator->haveCommonPrimeFactor(A, B))
+			{
+				for (int C = 2; C < 10; C++) {
+					if (_bealCalculator->haveCommonPrimeFactor(_bealCalculator->getGreatestCommonDivisior(A, B), C))
+					{
+						for (int x = 3; x < 10; x++) {
+							for (int y = 3; y < 10; y++) {
+								for (int z = 3; z < 10; z++) {
+									if (_bealCalculator->isNumberSetFitsBealConjecture(A, B, C, x, y, z)) {
+										BealData bealData(A, B, C, x, y, z);
+										_bealCalculator->checkAndAddBNTToMinimumVec(bnts, bealData, BNT_TO_FIND);
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+
+		}
+	}
 
     for (auto& bealData : bnts) {
         BealData* data = new BealData(bealData.A, bealData.B, bealData.C, bealData.x, bealData.y, bealData.z);
--- CSC8501_BealConjecture/QuestionTwo.cpp
+++ CSC8501_BealConjecture/QuestionTwo.cpp
@@ -14,17 +14,18 @@ QuestionTwo::QuestionTwo(const char* questionText, std::vector<const char*>& ans
 void QuestionTwo::findAnswer() {
 	std::vector<BealData> bnts;
 
-	for (int A = 2; A < 20; A++) {
+	for (int A = 2; A < 11; A++) {
 		for (int B = 2; B < A + 1; B++) {
 			if (_bealCalculator->haveCommonPrimeFactor(A, B))
 			{
-				for (int C = 2; C < 20; C++) {
+				for (int C = 2; C < 11; C++) {
 					if (_bealCalculator->haveCommonPrimeFactor(_bealCalculator->getGreatestCommonDivisior(A, B), C))
 					{
-						for (int x = 3; x < 20; x++) {
-							for (int y = 3; y < 20; y++) {
-								for (int z = 3; z < 20; z++) {
-									if (_bealCalculator->haveCommonPrimeFactor(A, C) && _bealCalculator->isNumberSetFitsBealConjecture(A, B, C, x, y, z) && isPrime(A + B + C + x + y + z)) {
+						for (int x = 3; x < 11; x++) {
+							for (int y = 3; y < 11; y++) {
+								for (int z = 3; z < 11; z++) {
+									int tempBnt = A + B + C + x + y + z;
+									if (isPrime(tempBnt) && _bealCalculator->isNumberSetFitsBealConjecture(A, B, C, x, y, z)) {
 										BealData bealData(A, B, C, x, y, z);
 										_bealCalculator->checkAndAddBNTToMinimumVec(bnts, bealData, BNT_TO_FIND);
 									}
@@ -32,12 +33,10 @@ void QuestionTwo::findAnswer() {
 							}
 						}
 					}
-
 				}
 			}
 
 		}
-
 	}
 
 	for (auto& bealData : bnts) {
